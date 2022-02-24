a6652ba22ffdf5b31b62b06255389ad38ca30945
root <root@firewall.envigo.net>
2022-02-24 22:46:40 +0100

    ..

--- wosproxy.py
+++ wosproxy.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""
 
+Copyright (c) 2016 Ad Schellevis <ad@opnsense.org>
+All rights reserved.
+
+"""
+
+import os
 import socket
 from _thread import *
 import threading
+
+
+socket_filename = '/var/run/wosproxy.socket'
 
 print_lock = threading.Lock()
 
@@ -18,19 +28,42 @@ def threaded(c):
     c.close()        
 
 
+def leave(s):
+    s.close()
+    if(os.path.exists(socket_filename)):
+        os.remove(socket_filename)    
+
 appname = "wosproxy"
-host = ""
+host = "12"
 port = 12345
-s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-s.bind((host, port))
-print("%s is listining on port %d", appname, port)
 
-while True:
-    c, addr = s.accept()
+try:
+    try:
+        os.unlink(socket_filename)
+    except OSError:
+        if os.path.exists(socket_filename):
+            print('socket exists')
+            raise                
 
-    print_lock.acquire()
-    print('Connected to:', addr[0], ':', addr[1])
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    s.bind(socket_filename)
+    os.chmod(socket_filename, 0o666)
+    s.listen(20)
+    print(appname, ' is listining on socket ', socket_filename)
+
+    while True:
+        c, addr = s.accept()
+
+        print_lock.acquire()
+        #print('Connected to:', addr[0], ':', addr[1])
     
-    start_new_thread(threaded,(c,))
+        start_new_thread(threaded,(c,))
 
-s.close()
+except KeyboardInterrupt:
+    #exit key
+    leave(s)
+
+except SystemExit:
+    #System exit
+    leave(s)    
+
