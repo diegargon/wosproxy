0134f36a02f081d76d9bfa7a4bb6e5aa5ffcd5e8
diego <diego@envigo.net>
2022-02-26 14:35:55 +0100

    init

--- wosproxy.py
+++ wosproxy.py
@@ -7,64 +7,32 @@ All rights reserved.
 
 """
 
-import os
-import socket
-from _thread import *
-import threading
+import sys, time
+from daemon import Daemon
+from wosmain import wos_main
+
+#Overwrite daemon run method
+class WosDaemon(Daemon):
+        def run(self):
+                wos_main()
 
 
-socket_filename = '/var/run/wosproxy.socket'
-
-print_lock = threading.Lock()
-
-def threaded(c):
-    while True:
-        data = c.recv(1024)
-        if not data:
-            print('no data')
-
-            print_lock.release()
-            break
-        # reverse data = data[::~1]
-    c.close()        
-
-
-def leave(s):
-    s.close()
-    if(os.path.exists(socket_filename)):
-        os.remove(socket_filename)    
-
-appname = "wosproxy"
-host = "12"
-port = 12345
-
-try:
-    try:
-        os.unlink(socket_filename)
-    except OSError:
-        if os.path.exists(socket_filename):
-            print('socket exists')
-            raise                
-
-    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    s.bind(socket_filename)
-    os.chmod(socket_filename, 0o666)
-    s.listen(20)
-    print(appname, ' is listining on socket ', socket_filename)
-
-    while True:
-        c, addr = s.accept()
-
-        print_lock.acquire()
-        #print('Connected to:', addr[0], ':', addr[1])
-    
-        start_new_thread(threaded,(c,))
-
-except KeyboardInterrupt:
-    #exit key
-    leave(s)
-
-except SystemExit:
-    #System exit
-    leave(s)    
-
+if len(sys.argv) == 2 and sys.argv[1] == '-nd':
+    wos_main()
+elif len(sys.argv) == 3 and sys.argv[1] == '-d':      
+    daemon = WosDaemon('/tmp/wosd.pid')
+    if 'start' == sys.argv[2]:
+        daemon.start()
+    elif 'stop' == sys.argv[2]:
+        daemon.stop()        
+    elif 'restart' == sys.argv[2]:
+        daemon.restart()
+    else:
+        print("Unknown option")
+        sys.exit(2)
+    sys.exit(0)
+else:
+    print ("Usage:")
+    print ("%s -nd (No daemon)" % sys.argv[0])
+    print ("%s -d start|stop|restart (daemon mode)" % sys.argv[0])
+    sys.exit(2)    
